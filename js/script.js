698a2123b70a926e605fccc63e0b4f14605e1917
IVAN\123 <ivan09031@gmail.com>
2018-02-18 01:11:00 +0700

    next

--- js/script.js
+++ js/script.js
@@ -4,30 +4,50 @@
 
 	var startGame = document.getElementById('btn-start'),
 		fieldStart = document.getElementById('container__start'),
-		fieldGame = document.getElementById('container__game');
+		fieldGame = document.getElementById('container__game'),
+		clickedCard = {};
+		
 
 	startGame.addEventListener("click", function() {
 		fieldStart.style.cssText = "display: none";
 		fieldGame.style.cssText = "display: block";
-		let img = "<img src=\"img/back.gif\" class=\"card\">";
+		let img = "<div class=\"cards\"><img src=\"img/back.gif\" class=\"card\"></div>";
 		let field = document.getElementById('field__cards');
 		field.innerHTML = img.repeat(18);
+		
+		clickedCard = document.getElementsByClassName('cards');
+		eventOnClick(clickedCard);
+
 	});
 
-	let clickCard = document.getElementById('field__cards');
-	clickCard.addEventListener("click", function(event) {
-		let currCard = event.target;
-		let acc = 0;
-		if (!currCard.classList.contains('active')) {
-			// if (acc <= 2) {
-				currCard.classList.add('active');
-				event.stopPropagation();
-				// acc++;
-			// }
-			// else event.stopPropagation();
-		}
-		else currCard.classList.remove('active');
-	});
+	function eventOnClick(card) {
+		Array.from(card).forEach(function(element) {
+     		element.addEventListener('click', function(event) {
+				let currCard = event.currentTarget;
+				currCard.classList.toggle("active");
+      		});
+
+    	});
+	};
+	
+
+
+
+	// clickCard.addEventListener("click", function(event) {
+	// 	let currCard = event.target;
+	// 	event.stopPropagation();
+	// 	acc++;
+	// 	console.log(currCard, acc);
+	// 	if (!currCard.classList.contains('active')) {
+	// 		// if (acc <= 2) {
+	// 			currCard.classList.add('active');
+	// 			event.stopPropagation();
+	// 			// acc++;
+	// 		// }
+	// 		// else event.stopPropagation();
+	// 	}
+	// 	else currCard.classList.remove('active');
+	// });
 
 	
 })();
