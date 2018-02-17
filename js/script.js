ff31ed67a234d5aea5bc6bb4f0df85d9dc63b9f2
IVAN\123 <ivan09031@gmail.com>
2018-02-17 13:12:34 +0700

    second

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 (function(){
-	
+
 	"use strict"
 
 	var startGame = document.getElementById('btn-start'),
@@ -9,14 +9,25 @@
 	startGame.addEventListener("click", function() {
 		fieldStart.style.cssText = "display: none";
 		fieldGame.style.cssText = "display: block";
-
-		for (let i = 0; i < 18; i++) {
-			let li = document.createElement('li'),
-				div = document.createElement('div');
-			li.className = 'field__card';
-			div.className = 'card';
-			document.getElementById('past').appendChild(li).appendChild(div);
-		}
+		let img = "<img src=\"img/back.gif\" class=\"card\">";
+		let field = document.getElementById('field__cards');
+		field.innerHTML = img.repeat(18);
 	});
 
+	let clickCard = document.getElementById('field__cards');
+	clickCard.addEventListener("click", function(event) {
+		let currCard = event.target;
+		let acc = 0;
+		if (!currCard.classList.contains('active')) {
+			// if (acc <= 2) {
+				currCard.classList.add('active');
+				event.stopPropagation();
+				// acc++;
+			// }
+			// else event.stopPropagation();
+		}
+		else currCard.classList.remove('active');
+	});
+
+	
 })();
