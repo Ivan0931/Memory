e3eab32549cfa260c3be4710bed6c44ab455628a
IVAN\123 <ivan09031@gmail.com>
2018-02-23 23:02:53 +0700

    some changes

--- js/script.js
+++ js/script.js
@@ -1,53 +1,95 @@
-(function(){
+{
 
 	"use strict"
+	const 	cardName = ["C", "D", "H", "S"],
+			startGame = document.getElementById('btn-start'),
+			fieldStart = document.getElementById('container__start'),
+			fieldGame = document.getElementById('container__game'),
+			field = document.getElementById('field__cards'),
+			clickedCard = document.getElementsByClassName('cards');
+//<img src=\"\" class=\"card-face\">
+	var img = `<div class=\"cards\">
+					<img src=\"img/back.gif\" class=\"card-back\">
+					
+				</div>`,
+		cardDeck = [],
+		newArr = [],
+		acc = 1;
 
-	var startGame = document.getElementById('btn-start'),
-		fieldStart = document.getElementById('container__start'),
-		fieldGame = document.getElementById('container__game'),
-		clickedCard = {};
-		
+	for (let i = 2; i < 15; i++) {
+		for (let n = 0; n < 4; n++) {
+			cardDeck.push( {name: i + cardName[n], src: `img/${ i + cardName[n] }.png`} );
+		}
+	};
+
+	// function shuffle(array) {
+	// 	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// 	  // While there remain elements to shuffle...
+	// 	while (0 !== currentIndex) {
+
+	// 	    // Pick a remaining element...
+	// 	    randomIndex = Math.floor(Math.random() * currentIndex);
+	// 	    currentIndex -= 1;
+
+	// 	    // And swap it with the current element.
+	// 	    temporaryValue = array[currentIndex];
+	// 	    array[currentIndex] = array[randomIndex];
+	// 	    array[randomIndex] = temporaryValue;
+	// 	};
+	// 	return array;
+	// };
+
+	var shuffled = cardDeck.map((a) => 
+								[Math.random(),a])
+								.sort((a,b) => a[0]-b[0])
+								.map((a) => a[1])
+							.slice(0, 18);
+
+	console.log(shuffled);
+
 
 	startGame.addEventListener("click", function() {
 		fieldStart.style.cssText = "display: none";
 		fieldGame.style.cssText = "display: block";
-		let img = "<div class=\"cards\"><img src=\"img/back.gif\" class=\"card\"></div>";
-		let field = document.getElementById('field__cards');
 		field.innerHTML = img.repeat(18);
-		
-		clickedCard = document.getElementsByClassName('cards');
+
 		eventOnClick(clickedCard);
 
+		Array.from(clickedCard).forEach( function(element, index) {
+			// let img = document.createElement('img');
+			// img.src = newArr[index].src;
+			// img.className = "card-face";
+			// element.appendChild(img)
+		});
 	});
+
+	
 
 	function eventOnClick(card) {
 		Array.from(card).forEach(function(element) {
+
      		element.addEventListener('click', function(event) {
-				let currCard = event.currentTarget;
-				currCard.classList.toggle("active");
+     			
+     			let currCard = event.currentTarget;
+     			if (acc <= 2) {
+     				currCard.classList.toggle('active');
+     				acc++;
+     			}
+     			else {
+     				event.preventDefault();
+     				Array.from(card).forEach(function(item) {
+     					item.classList.remove('active');
+     				});
+     				acc = 1;
+				}
+
       		});
 
     	});
 	};
-	
 
-
-
-	// clickCard.addEventListener("click", function(event) {
-	// 	let currCard = event.target;
-	// 	event.stopPropagation();
-	// 	acc++;
-	// 	console.log(currCard, acc);
-	// 	if (!currCard.classList.contains('active')) {
-	// 		// if (acc <= 2) {
-	// 			currCard.classList.add('active');
-	// 			event.stopPropagation();
-	// 			// acc++;
-	// 		// }
-	// 		// else event.stopPropagation();
-	// 	}
-	// 	else currCard.classList.remove('active');
-	// });
 
 	
-})();
+	
+};
