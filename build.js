649a09d66c160072d3745833aaa2744412001daf
Ivan0931 <atleast111@tutanota.com>
2022-10-19 10:40:11 +0700

    move tests folder to the root

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ const destDir = 'build'
 try {
   fs.rmSync('./build', { recursive: true, force: true })
   fs.mkdirSync(destDir)
-  exec(`rsync -ar ${srcDir}/ ${destDir} && rm -rf ${destDir}/__tests__`)
+  exec(`rsync -ar ${srcDir}/ ${destDir}`)
 } catch (err) {
   console.error(err)
 }
